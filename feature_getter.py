4987d1824a07d1b2c2286bfc4ed3989e93ad02aa
Jordi Granja Bayot <=>
2024-04-10 10:18:42 +0200

    feature_getter_inicial

--- feature_getter.py
+++ feature_getter.py
@@ -1,2 +1,60 @@
+import re
+import unicodedata
+
+from nltk.tag.api import TaggerI
+
+try:
+    import pycrfsuite
+except ImportError:
+    pass
+
+
 class Feature_getter:
-    
+    def __init__(self, POS,length,):
+        pass
+    def __call__(self, tokens, idx):
+        """
+        Extract basic features about this word including
+            - Current word
+            - is it capitalized?
+            - Does it have punctuation?
+            - Does it have a number?
+            - Suffixes up to length 3
+
+        Note that : we might include feature over previous word, next word etc.
+
+        :return: a list which contains the features
+        :rtype: list(str)
+        """
+        token = tokens[idx]
+
+        feature_list = []
+
+        if not token:
+            return feature_list
+
+        # Capitalization
+        if token[0].isupper():
+            feature_list.append("CAPITALIZATION")
+
+        # Number
+        if re.search(self._pattern, token) is not None:
+            feature_list.append("HAS_NUM")
+
+        # Punctuation
+        punc_cat = {"Pc", "Pd", "Ps", "Pe", "Pi", "Pf", "Po"}
+        if all(unicodedata.category(x) in punc_cat for x in token):
+            feature_list.append("PUNCTUATION")
+
+        # Suffix up to length 3
+        if len(token) > 1:
+            feature_list.append("SUF_" + token[-1:])
+        if len(token) > 2:
+            feature_list.append("SUF_" + token[-2:])
+        if len(token) > 3:
+            feature_list.append("SUF_" + token[-3:])
+
+        feature_list.append("WORD_" + token)
+
+        return feature_list
+
